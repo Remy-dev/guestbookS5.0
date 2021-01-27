57503cf57631cc3c05c9f6b1bd4f9e7d6acce274
Remy-dev <g75v@protonmail.com>
2021-01-27 14:37:12 +0100

    mise en place du slug

---
+++ src/EventSubscriber/TwigEventSubscriber.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\EventSubscriber;
+
+use App\Repository\ConferenceRepository;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpKernel\Event\ControllerEvent;
+use Twig\Environment;
+
+class TwigEventSubscriber implements EventSubscriberInterface
+{
+    private $conferenceRepository;
+    private $twig;
+
+    public function __construct(Environment $twig, ConferenceRepository $conferenceRepository){
+        $this->conferenceRepository = $conferenceRepository;
+        $this->twig = $twig;
+    }
+    public function onKernelController(ControllerEvent $event)
+    {
+        $this->twig->addGlobal('conferences', $this->conferenceRepository->findAll());
+    }
+
+    public static function getSubscribedEvents()
+    {
+        return [
+            'kernel.controller' => 'onKernelController',
+        ];
+    }
+}
