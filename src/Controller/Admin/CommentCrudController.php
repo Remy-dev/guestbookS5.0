878018f2cb741c301369ff47991ac47047a94e70
Remy-dev <g75v@protonmail.com>
2021-01-28 14:25:24 +0100

    réglage upload photo dans EasyAdmin interface

--- src/Controller/Admin/CommentCrudController.php
+++ src/Controller/Admin/CommentCrudController.php
@@ -3,9 +3,13 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Comment;
+use App\Entity\Conference;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\EmailField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ImageField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 
 class CommentCrudController extends AbstractCrudController
 {
@@ -17,16 +21,19 @@ class CommentCrudController extends AbstractCrudController
     public function configureCrud(Crud $crud): Crud
     {
         return $crud
-            ->setDateTimeFormat(DateTimeField::FORMAT_MEDIUM);
+            ->setDateTimeFormat(DateTimeField::FORMAT_MEDIUM)
+            ->setDefaultSort(['createdAt' => 'DESC']);
+
     }
-    /*
+
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
+             TextField::new('author'),
+             EmailField::new('email'),
+             TextField::new('text'),
+             ImageField::new('photoFilename')->setLabel('Photo')->setUploadDir('public/uploads/photos')
+
         ];
     }
-    */
 }
